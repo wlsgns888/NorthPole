ab0fbb6feccb572381014298b1f703b77746cd73
jinhoon <jh101368@gmail.com>
2021-03-26 00:31:30 +0900

    first commit test

---
+++ data_EDA.py
@@ -0,0 +1,8 @@
+import os
+import pandas as pd
+
+data_dir = r'D:\Users\jh\Google 드라이브\local_dataset\north'
+data_path = r'D:\Users\jh\Google 드라이브\local_dataset\north\train.cvs'
+data_train_dir = r'D:\Users\jh\Google 드라이브\local_dataset\north\train'
+
+if __name__ == '__main__':
